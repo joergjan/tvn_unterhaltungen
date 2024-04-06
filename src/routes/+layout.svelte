344aa5d99aca16424e0f7c45bdb53aedcd06a623
Jan Jörg <jan.joerg.jj@gmail.com>
2024-04-06 11:16:05 +0200

    layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
   import "../app.css";
+  import Logo from "$lib/components/Logo.svelte";
 </script>
 
 <main class="p-5 break-all">
+  <div class="flex items-center justify-center mb-5">
+    <Logo className="h-16 md:h-20" />
+  </div>
+
   <slot />
 </main>
