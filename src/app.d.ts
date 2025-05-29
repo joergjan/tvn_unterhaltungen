d3877d48502dacbcc036d5a11ee26bd71d5c668e
Jan Jörg <jan.joerg.jj@gmail.com>
2025-05-29 10:48:09 +0200

    bugfixes

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,11 @@
-import type { LoaderLocals } from '@sanity/svelte-loader';
-import { Anlass, BlogPost, Riege, Person, Gallery } from '$lib/sanity/queries';
+import type { LoaderLocals } from "@sanity/svelte-loader";
 
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		interface Locals extends LoaderLocals {}
-	}
-	type Anlass = Anlass;
-	type BlogPost = BlogPost;
-	type Riege = Riege;
-	type Person = Person;
-	type Gallery = Gallery;
+  namespace App {
+    interface Locals extends LoaderLocals {}
+  }
 }
 
 export {};
